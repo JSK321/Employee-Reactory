936f08ce105df9026850cca90f8441972b8ef2cd
Jae Kim <jaeshinkim321@gmail.com>
2020-11-07 14:23:14 -0800

    refactored code, search term updates state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
-import React from "react";
-import OmdbContainer from "./components/OmdbContainer";
-// import EmployeeCard from "./components/EmployeeCard";
-// import NavBar from "./components/NavBar";
+import React, { Component } from "react";
+import NavBar from "./components/NavBar";
+import EmployeeCard from "./components/EmployeeCard";
+import API from "./utils/API";
 
-function App() {
-  return <OmdbContainer />
-  
+class App extends Component {
+    state = {
+        result: [],
+        search:""
+    }
+
+    componentDidMount() {
+        this.getEmployees();
+    }
+
+    getEmployees() {
+        API.search().then(res =>
+            this.setState({ result: res.data.results })
+        )
+    }
+
+    handleInputChange = event => {
+        const {name, value} = event.target;
+        this.setState({
+          [name]: value
+        });
+    };
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        let keyword = event.target.value;
+        this.setState({ search: keyword })
+    }
+
+    render() {
+
+        return (
+            <div>
+                <NavBar
+                   handleInputChange={this.handleInputChange} handleFormSubmit={this.handleFormSubmit}
+                />
+                <div className="row">
+                    {this.state.result.map(result => (
+                        <EmployeeCard
+                            key={result.id.value}
+                            firstName={result.name.first}
+                            lastName={result.name.last}
+                            phone={result.phone}
+                            email={result.email}
+                            age={result.dob.age}
+                            image={result.picture.large}
+                        />
+                    ))}
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,27 +1,57 @@
 import React from 'react'
 import "./styles.css";
 
-
 export default function NavBar(props) {
     return (
         <div>
             <nav className="navbar">
                 <h1 className="mx-auto">Employee Directory</h1>
-                <form className="form-inline">
+                <form className="form-inline" onSubmit={props.handleFormSubmit}>
                     <input
-                        // onChange={props.handleInputChange}
-                        // value={props.value}
-                        name="searchEmp"
+                        onChange={props.handleInputChange}
+                        value={props.search}
+                        name="search"
                         className="form-control"
                         type="search"
                         placeholder="Search Employee"
                         aria-label="Search"
                     />
-                <button className="searchBtn">
-                    Search
-                </button>
                 </form>
             </nav>
         </div>
     )
 }
+
+
+// import React, { Component } from 'react'
+
+// export default class NavBar extends Component {
+//     state = {
+//         search: ""
+//     }
+
+//     searchEmp = event => {
+//         let keyword = event.target.value;
+//         this.setState({search: keyword})
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//             <nav className="navbar">
+//                 <h1 className="mx-auto">Employee Directory</h1>
+//                 <form className="form-inline">
+//                     <input
+//                         onChange={this.searchEmp}
+//                         name="search"
+//                         className="form-control"
+//                         type="search"
+//                         placeholder="Search Employee"
+//                         aria-label="Search"
+//                     />
+//                 </form>
+//             </nav>
+//         </div>
+//         )
+//     }
+// }
